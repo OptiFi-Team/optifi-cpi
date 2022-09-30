8c7e50d0c523bec544c74777f6686cca33f0a5a6
Wayne Kuo <wayne.k@alphabit.software>
2022-09-30 17:47:30 +0800

    Remove clock

--- programs/optifi-cpi/src/instructions/user/user_margin.rs
+++ programs/optifi-cpi/src/instructions/user/user_margin.rs
@@ -5,12 +5,12 @@ pub struct MarginContext<'info> {
     /// optifi_exchange account
     pub optifi_exchange: Box<Account<'info, Exchange>>,
     /// margin stress account
-    #[account(constraint = margin_stress_account.optifi_exchange == optifi_exchange.key())]
+    #[account(
+        constraint = margin_stress_account.optifi_exchange == optifi_exchange.key(),
+        constraint = !margin_stress_account.is_timeout() @ OptifiErrorCode::TimeOut,
+    )]
     pub margin_stress_account: Box<Account<'info, MarginStressAccount>>,
     /// user's optifi account
     #[account(mut, constraint = user_account.optifi_exchange == optifi_exchange.key())]
     pub user_account: Box<Account<'info, UserAccount>>,
-
-    /// Clock to get the timestamp
-    pub clock: Sysvar<'info, Clock>,
 }
